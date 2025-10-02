09e8f29e2a864e52dae3ba1d4c4fd8a78862ae9a
Muhammed Fayiz <fayiz149165@gmail.com>
2025-10-02 23:13:12 +0530

    add the task bar

--- router.js
+++ router.js
@@ -7,17 +7,16 @@ const loginPage=path.join(__dirname,'pages','loginpage.ejs')
 router.get('/',(req,res)=>{
 res.render('loginpage')
 })
-router.post('/loginpost',(req,res)=>{
- 
+router.post('/loginpost', (req, res) => {
+  const { email, password } = req.body;
 
-const {email,password}=req.body;
+  if (email === "fayiz@gmail.com" && password === "123") {
+    res.render('home');
+  } else {
+    res.render('loginpage', { error: "Invalid email or password" });
+  }
+});
 
-if(email=="fayiz@gmail.com"&&password=="123"){
-    res.render('home')
-}else{
-    res.redirect('/')
-}
-})
 
 
 module.exports=router
